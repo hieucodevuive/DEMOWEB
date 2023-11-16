b166cb942caa382a33224b5bf1cb5304d9156f13
Hieu_funny <hieupham.code@gmail.com>
2023-11-17 02:45:28 +0700

    filter product
    By vendor and category

--- core/views.py
+++ core/views.py
@@ -150,14 +150,15 @@ def filter_product(request):
     categories = request.GET.getlist("category[]")
     vendors = request.GET.getlist("vendor[]")
     
-    products = Product.objects.filter(product_status="published").order_by("-date").distinct()
+    products = Product.objects.filter(product_status="published").order_by("-id").distinct()
     
     if len(categories) > 0:
         products = products.filter(category__id__in = categories).distinct()
     
     
     if len(vendors) > 0:
-        products = products.filter(vendor__id__in = vendors).distinct()
+        products = products.filter(vendor__id__in=vendors).distinct() 
     
-    data = render_to_string("core/async/product-list.html", {"products":products,})
-    return JsonResponse({"data":data,})
+    
+    data = render_to_string("core/async/product-list.html", {"products": products})
+    return JsonResponse({"data": data})
--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -26,26 +26,51 @@ $("#commentForm").submit(function(e){
 
 
 
+// $(document).ready(function (){
+//     $(".filter-checkbox").on("click", function(){
+//         let filter_object = {}
+//         $(".filter-checkbox").each(function(){
+//             let filter_value = $(this).val()
+//             let filter_key = $(this).data("filter")
+
+//             filter_object[filter_key] = Array.from(document.querySelectorAll('input[data-filter=' + filter_key + ']:checked')).map(function(element){
+//                 return element.value
+//             })
+//         $.ajax({
+//             url: '/filter-products',
+//             data: filter_object,
+//             dataType: 'json',
+//             beforeSend: function() {
+//             },
+//             success: function(response) {
+//                 $("#filtered-product").html(response.data)
+//             },
+//         })
+//         })
+//     })
+// })
+
 $(document).ready(function (){
     $(".filter-checkbox").on("click", function(){
+
         let filter_object = {}
+
         $(".filter-checkbox").each(function(){
             let filter_value = $(this).val()
-            let filter_key = $(this).data("filter")
-
-            filter_object[filter_key] = Array.from(document.querySelectorAll('input[data-filter='+ filter_key+']:checked')).map(function(e){
-                return e.value
-            })
-            $.ajax({
-                url: '/filter-products',
-                data: filter_object,
-                dataType: 'json',
-                beforeSend: function() {
-                },
-                success: function(response) {
-                    $("#filltered-product").html(response.data)
-                },
-            })
+            let filter_key = $(this).data("filter") 
+       
+            filter_object[filter_key] = Array.from(document.querySelectorAll('input[data-filter=' + filter_key + ']:checked')).map(function(element){
+                return element.value
+            })  
+        })
+        $.ajax({
+            url: '/filter-products',
+            data: filter_object,
+            dataType: 'json',
+            success: function(response){
+                $(".totall-product").hide()
+                $("#filtered-product").html(response.data)
+            }
         })
     })
 })
